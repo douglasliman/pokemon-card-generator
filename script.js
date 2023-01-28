1ea07e79a49d89df5e78631aeb985a2bb5ad182d
Douglas Queiroz <douglas.lima.queiroz@gmail.com>
2023-01-27 21:47:37 -0300

    generated-card-update

--- script.js
+++ script.js
@@ -24,27 +24,61 @@ const btn = document.getElementById("btn");
 let getPokeData = () => {
   //Gera um numero aleatório entre 1 e 150
   let id = Math.floor(Math.random() * 150) + 1;
-//   console.log(id);
+  //   console.log(id);
 
   //combina a  url da pokeapi com o pokemon id
   const finalUrl = url + id;
-//   console.log(finalUrl);
+  //   console.log(finalUrl);
   //busca url gerado
-  
-    fetch (finalUrl)
-        .then((response) => response.json()).then((data) => {
-            generateCard(data)
-        });
+
+  fetch(finalUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      generateCard(data);
+    });
 };
 
 // ---------GERAR O CARD ---------------------
 
-// let generateCard = (data) => {
-//     //  Obtém os dados necessários e os atribui a variáveis
-//     console.log(data)
+let generateCard = (data) => {
+  //  Obtém os dados necessários e os atribui a variáveis
+  console.log(data);
+  const hp = data.stats[0].base_stat;
+  // console.log(hp);
+  const imgScr = data.sprites.other.dream_world.front_default;
+  const pokeName = data.name;
+  const statAttack = data.stats[1].base_stat;
+  const statDefense = data.stats[2].base_stat;
+  const statSpeed = data.stats[5].base_stat;
 
-// }
-
+  card.innerHTML = `
+                    <p class="hp">
+                    <span>HP</span>
+                    ${hp}
+                  </p>
+                  <img src="${imgScr}" alt="">
+                  <h2 class="poke-name">${pokeName}</h2>
+                  <div class="types">
+                    <span>Type 1</span>
+                    <span>Type 2</span>
+                  </div>
+                  <div class="stats">
+                  <div>
+                    <h3>${statAttack}</h3>
+                    <p>Attack</p>
+                  </div>
+                  <div>
+                    <h3>${statDefense}</h3>
+                    <p>Defense</p>
+                  </div>
+                  <div>
+                    <h3>${statSpeed}</h3>
+                    <p>Speed</p>
+                  </div>
+                  </div>
+                    
+  `;
+};
 
 btn.addEventListener("click", getPokeData);
-// window.addEventListener("load", getPokeData);
+window.addEventListener("load", getPokeData);
